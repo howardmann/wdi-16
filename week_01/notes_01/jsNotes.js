46696af61a2ce09a6203a06b959ed07330f35b95
Howard Mann <howardmann27@gmail.com>
2016-07-26 12:22:52 +1000

    add wdi-16 local to gitHub

---
+++ week_01/notes_01/jsNotes.js
@@ -0,0 +1,5 @@
+// Javascript variable naming conventions
+
+// var letters: for common variables
+// var $jQuery: for jQuery objects
+// var _underscore: for hidden, private and constant variables. Tell programmers not to change
