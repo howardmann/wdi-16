2e230769d7f638119b867f74e9e98b8ada67148b
Howard Mann <howardmann27@gmail.com>
2016-07-27 09:48:50 +1000

    add day03 warm up exercises

---
+++ warm_up/day_03/main.js
@@ -0,0 +1,44 @@
+console.log("Day 3 warm-up exercise");
+
+// If divisible by 3 print Pling, if divisible by 5 print Plang if divisible by 7 print Plong. Otherwise print number
+
+//Long way of doing it
+
+for (var num = 1; num <= 10; num++) {
+
+  if (num%3===0 && num%5===0 && num%7===0) {
+    console.log("PlingPlangPlong");
+  }
+  else if (num%3===0 && num%5===0) {
+    console.log("PlingPlang");
+  } else if (num%3===0 && num%7===0) {
+    console.log("PlingPlong");
+  } else if (num%5===0 && num%7===0) {
+    console.log("PlangPlong");
+  } else if (num%3===0) {
+    console.log('Pling');
+  } else if (num%5===0) {
+    console.log('Plang');
+  } else if (num%7===0) {
+    console.log('Plong');
+  } else {
+    console.log(num);
+  }
+
+}
+
+console.log("BREAK\n");
+
+
+// Find a better way without writing first three lines of code
+
+  var num = 35;
+  var string = "";
+  if (num%3===0) {
+    string+="Pling";
+  } if (num%5===0) {
+    string+="Plang";
+  } if (num%7===0) {
+    string+="Plong";
+  }
+  console.log(string || num);
